6a60621529e289cd696e209eddab00e28a9b84b3
Eamon <chenlulei@live.com>
2018-06-20 11:51:14 +0800

    add some method for networking

--- networking/networking.go
+++ networking/networking.go
@@ -1,10 +1,17 @@
 package networking
 
 import (
-	"net/http"
 	"bytes"
+	"errors"
+	"net/http"
+	"strconv"
+	"time"
+
+	httpCli "github.com/gojektech/heimdall"
+	"honeywell.com/husplus/onvif-profiles-service/config"
 )
 
+//SendSoap message
 func SendSoap(endpoint, message string) (*http.Response, error) {
 	httpClient := new(http.Client)
 
@@ -13,5 +20,48 @@ func SendSoap(endpoint, message string) (*http.Response, error) {
 		return resp, err
 	}
 
-	return resp,nil
+	return resp, nil
+}
+
+//HTTPPostEx use heimdall for circle break、 timeout、retry
+func HTTPPostEx(endpoint, message string, timeout int) (*http.Response, error) {
+	client := httpCli.NewHTTPClient(time.Duration(timeout) * time.Second)
+	headers := http.Header{}
+
+	postBody := []byte(message)
+	headers.Add("Content-Type", "text/xml; charset=utf-8")
+	headers.Add("Content-Length", strconv.Itoa(len(postBody)))
+
+	resp, err := client.Post(endpoint, bytes.NewBuffer(postBody), headers)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+
+}
+
+//SendSoapEx to post a request
+func SendSoapEx(connect ConnectParameter, message string) ([]byte, error) {
+	var url string
+	if len(connect.Address) > 0 {
+		url = connect.Address
+	} else {
+		url = config.HTTPPrefix + connect.Host + connect.URL
+	}
+
+	resp, err := HTTPPostEx(url, message, connect.Timeout)
+
+	if err != nil {
+		//	fmt.Println(err.Error())
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+	var buf = new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	return buf.Bytes(), nil
 }
---
+++ networking/type.go
@@ -0,0 +1,9 @@
+package networking
+
+//ConnectParameter Host/url & timeout
+type ConnectParameter struct {
+	Host    string
+	URL     string
+	Address string
+	Timeout int
+}
