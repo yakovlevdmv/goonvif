c572808563ea9ca1c5daad55f92b1054ceee1f14
rikugun <v5.rikugun@gmail.com>
2020-01-15 17:29:46 +0800

    feat:add omitempty

--- Device.go
+++ Device.go
@@ -2,36 +2,36 @@ package goonvif
 
 import (
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"github.com/beevik/etree"
-	"github.com/yakovlevdmv/gosoap"
-	"strconv"
-	"net/http"
-	"io/ioutil"
+	"github.com/rikugun/goonvif/Device"
+	"github.com/rikugun/goonvif/networking"
 	"github.com/yakovlevdmv/WS-Discovery"
-	"strings"
-	"github.com/yakovlevdmv/goonvif/Device"
-	"errors"
+	"github.com/yakovlevdmv/gosoap"
+	"io/ioutil"
+	"net/http"
 	"reflect"
-	"github.com/yakovlevdmv/goonvif/networking"
+	"strconv"
+	"strings"
 )
 
-var Xlmns = map[string]string {
-	"onvif":"http://www.onvif.org/ver10/schema",
-	"tds":"http://www.onvif.org/ver10/device/wsdl",
-	"trt":"http://www.onvif.org/ver10/media/wsdl",
-	"tev":"http://www.onvif.org/ver10/events/wsdl",
-	"tptz":"http://www.onvif.org/ver20/ptz/wsdl",
-	"timg":"http://www.onvif.org/ver20/imaging/wsdl",
-	"tan":"http://www.onvif.org/ver20/analytics/wsdl",
-	"xmime":"http://www.w3.org/2005/05/xmlmime",
-	"wsnt":"http://docs.oasis-open.org/wsn/b-2",
-	"xop":"http://www.w3.org/2004/08/xop/include",
-	"wsa":"http://www.w3.org/2005/08/addressing",
-	"wstop":"http://docs.oasis-open.org/wsn/t-1",
-	"wsntw":"http://docs.oasis-open.org/wsn/bw-2",
-	"wsrf-rw":"http://docs.oasis-open.org/wsrf/rw-2",
-	"wsaw":"http://www.w3.org/2006/05/addressing/wsdl",
+var Xlmns = map[string]string{
+	"onvif":   "http://www.onvif.org/ver10/schema",
+	"tds":     "http://www.onvif.org/ver10/device/wsdl",
+	"trt":     "http://www.onvif.org/ver10/media/wsdl",
+	"tev":     "http://www.onvif.org/ver10/events/wsdl",
+	"tptz":    "http://www.onvif.org/ver20/ptz/wsdl",
+	"timg":    "http://www.onvif.org/ver20/imaging/wsdl",
+	"tan":     "http://www.onvif.org/ver20/analytics/wsdl",
+	"xmime":   "http://www.w3.org/2005/05/xmlmime",
+	"wsnt":    "http://docs.oasis-open.org/wsn/b-2",
+	"xop":     "http://www.w3.org/2004/08/xop/include",
+	"wsa":     "http://www.w3.org/2005/08/addressing",
+	"wstop":   "http://docs.oasis-open.org/wsn/t-1",
+	"wsntw":   "http://docs.oasis-open.org/wsn/bw-2",
+	"wsrf-rw": "http://docs.oasis-open.org/wsrf/rw-2",
+	"wsaw":    "http://www.w3.org/2006/05/addressing/wsdl",
 }
 
 type DeviceType int
@@ -44,7 +44,7 @@ const (
 )
 
 func (devType DeviceType) String() string {
-	stringRepresentation := []string {
+	stringRepresentation := []string{
 		"NetworkVideoDisplay",
 		"NetworkVideoStorage",
 		"NetworkVideoAnalytics",
@@ -61,28 +61,26 @@ func (devType DeviceType) String() string {
 
 //deviceInfo struct contains general information about ONVIF device
 type deviceInfo struct {
-	Manufacturer string
-	Model string
+	Manufacturer    string
+	Model           string
 	FirmwareVersion string
-	SerialNumber string
-	HardwareId string
-
+	SerialNumber    string
+	HardwareId      string
 }
 
 //deviceInfo struct represents an abstract ONVIF device.
 //It contains methods, which helps to communicate with ONVIF device
 type device struct {
-
-	xaddr string
-	login string
+	xaddr    string
+	login    string
 	password string
 
 	endpoints map[string]string
-	info deviceInfo
-
+	info      deviceInfo
+	Profiles  []string
 }
 
-func (dev *device)GetServices() map[string]string {
+func (dev *device) GetServices() map[string]string {
 	return dev.endpoints
 }
 
@@ -96,9 +94,9 @@ func readResponse(resp *http.Response) string {
 
 func GetAvailableDevicesAtSpecificEthernetInterface(interfaceName string) []device {
 	/*
-	Call an WS-Discovery Probe Message to Discover NVT type Devices
-	 */
-	devices := WS_Discovery.SendProbe(interfaceName, nil, []string{"dn:"+NVT.String()}, map[string]string{"dn":"http://www.onvif.org/ver10/network/wsdl"})
+		Call an WS-Discovery Probe Message to Discover NVT type Devices
+	*/
+	devices := WS_Discovery.SendProbe(interfaceName, nil, []string{"dn:" + NVT.String()}, map[string]string{"dn": "http://www.onvif.org/ver10/network/wsdl"})
 	nvtDevices := make([]device, 0)
 	////fmt.Println(devices)
 	for _, j := range devices {
@@ -144,22 +142,22 @@ func (dev *device) getSupportedServices(resp *http.Response) {
 	//resp, err := dev.CallMethod(Device.GetCapabilities{Category:"All"})
 	//if err != nil {
 	//	log.Println(err.Error())
-		//return
+	//return
 	//} else {
-		doc := etree.NewDocument()
+	doc := etree.NewDocument()
 
-		data, _ := ioutil.ReadAll(resp.Body)
+	data, _ := ioutil.ReadAll(resp.Body)
 
-		if err := doc.ReadFromBytes(data); err != nil {
-			//log.Println(err.Error())
-			return
-		}
-		services := doc.FindElements("./Envelope/Body/GetCapabilitiesResponse/Capabilities/*/XAddr")
-		for _, j := range services{
-			////fmt.Println(j.Text())
-			////fmt.Println(j.Parent().Tag)
-			dev.addEndpoint(j.Parent().Tag, j.Text())
-		}
+	if err := doc.ReadFromBytes(data); err != nil {
+		//log.Println(err.Error())
+		return
+	}
+	services := doc.FindElements("./Envelope/Body/GetCapabilitiesResponse/Capabilities/*/XAddr")
+	for _, j := range services {
+		////fmt.Println(j.Text())
+		////fmt.Println(j.Parent().Tag)
+		dev.addEndpoint(j.Parent().Tag, j.Text())
+	}
 	//}
 }
 
@@ -184,8 +182,8 @@ func NewDevice(xaddr string) (*device, error) {
 	return dev, nil
 }
 
-func (dev *device)addEndpoint(Key, Value string) {
-	dev.endpoints[Key]=Value
+func (dev *device) addEndpoint(Key, Value string) {
+	dev.endpoints[Key] = Value
 }
 
 //Authenticate function authenticate client in the ONVIF Device.
@@ -202,7 +200,6 @@ func (dev *device) GetEndpoint(name string) string {
 	return dev.endpoints[name]
 }
 
-
 func buildMethodSOAP(msg string) (gosoap.SoapMessage, error) {
 	doc := etree.NewDocument()
 	if err := doc.ReadFromString(msg); err != nil {
@@ -212,7 +209,6 @@ func buildMethodSOAP(msg string) (gosoap.SoapMessage, error) {
 	}
 	element := doc.Root()
 
-
 	soap := gosoap.NewEmptySOAP()
 	soap.AddBodyContent(element)
 	//soap.AddRootNamespace("onvif", "http://www.onvif.org/ver10/device/wsdl")
@@ -220,21 +216,24 @@ func buildMethodSOAP(msg string) (gosoap.SoapMessage, error) {
 	return soap, nil
 }
 
-
-
 //CallMethod functions call an method, defined <method> struct.
 //You should use Authenticate method to call authorized requests.
 func (dev device) CallMethod(method interface{}) (*http.Response, error) {
-	pkgPath := strings.Split(reflect.TypeOf(method).PkgPath(),"/")
+	pkgPath := strings.Split(reflect.TypeOf(method).PkgPath(), "/")
 	pkg := pkgPath[len(pkgPath)-1]
 
 	var endpoint string
 	switch pkg {
-		case "Device": endpoint = dev.endpoints["Device"]
-		case "Event": endpoint = dev.endpoints["Event"]
-		case "Imaging": endpoint = dev.endpoints["Imaging"]
-		case "Media": endpoint = dev.endpoints["Media"]
-		case "PTZ": endpoint = dev.endpoints["PTZ"]
+	case "Device":
+		endpoint = dev.endpoints["Device"]
+	case "Event":
+		endpoint = dev.endpoints["Event"]
+	case "Imaging":
+		endpoint = dev.endpoints["Imaging"]
+	case "Media":
+		endpoint = dev.endpoints["Media"]
+	case "PTZ":
+		endpoint = dev.endpoints["PTZ"]
 	}
 
 	//TODO: Get endpoint automatically
@@ -249,8 +248,8 @@ func (dev device) CallMethod(method interface{}) (*http.Response, error) {
 func (dev device) callNonAuthorizedMethod(endpoint string, method interface{}) (*http.Response, error) {
 	//TODO: Get endpoint automatically
 	/*
-	Converting <method> struct to xml string representation
-	 */
+		Converting <method> struct to xml string representation
+	*/
 	output, err := xml.MarshalIndent(method, "  ", "    ")
 	if err != nil {
 		//log.Printf("error: %v\n", err.Error())
@@ -258,8 +257,8 @@ func (dev device) callNonAuthorizedMethod(endpoint string, method interface{}) (
 	}
 
 	/*
-	Build an SOAP request with <method>
-	 */
+		Build an SOAP request with <method>
+	*/
 	soap, err := buildMethodSOAP(string(output))
 	if err != nil {
 		//log.Printf("error: %v\n", err)
@@ -267,21 +266,21 @@ func (dev device) callNonAuthorizedMethod(endpoint string, method interface{}) (
 	}
 
 	/*
-	Adding namespaces
-	 */
+		Adding namespaces
+	*/
 	soap.AddRootNamespaces(Xlmns)
 
 	/*
-	Sending request and returns the response
-	 */
+		Sending request and returns the response
+	*/
 	return networking.SendSoap(endpoint, soap.String())
 }
 
 //CallMethod functions call an method, defined <method> struct with authentication data
 func (dev device) callAuthorizedMethod(endpoint string, method interface{}) (*http.Response, error) {
 	/*
-	Converting <method> struct to xml string representation
-	 */
+		Converting <method> struct to xml string representation
+	*/
 	output, err := xml.MarshalIndent(method, "  ", "    ")
 	if err != nil {
 		//log.Printf("error: %v\n", err.Error())
@@ -289,8 +288,8 @@ func (dev device) callAuthorizedMethod(endpoint string, method interface{}) (*ht
 	}
 
 	/*
-	Build an SOAP request with <method>
-	 */
+		Build an SOAP request with <method>
+	*/
 	soap, err := buildMethodSOAP(string(output))
 	if err != nil {
 		//log.Printf("error: %v\n", err.Error())
@@ -298,14 +297,13 @@ func (dev device) callAuthorizedMethod(endpoint string, method interface{}) (*ht
 	}
 
 	/*
-	Adding namespaces and WS-Security headers
-	 */
+		Adding namespaces and WS-Security headers
+	*/
 	soap.AddRootNamespaces(Xlmns)
 	soap.AddWSSecurity(dev.login, dev.password)
 
-
 	/*
-	Sending request and returns the response
-	 */
+		Sending request and returns the response
+	*/
 	return networking.SendSoap(endpoint, soap.String())
 }
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/rikugun/goonvif
+
+go 1.13
+
+require (
+	github.com/beevik/etree v1.1.0
+	github.com/elgs/gostrgen v0.0.0-20161222160715-9d61ae07eeae // indirect
+	github.com/gin-gonic/gin v1.5.0
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b // indirect
+	github.com/yakovlevdmv/Golang-iso8601-duration v0.0.0-20180403125811-e5db0413b903
+	github.com/yakovlevdmv/WS-Discovery v0.0.0-20180512141937-16170c6c3677
+	github.com/yakovlevdmv/goonvif v0.0.0-20180517145634-8181eb3ef2fb
+	github.com/yakovlevdmv/gosoap v0.0.0-20180512142237-299a954b1c6d
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+)
